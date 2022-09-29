cb16274c0aca5ae9aa381c4f6b48a9155db8baad
Miguel Hernandez <mikeahm2506@hotmail.com>
2022-09-28 20:21:17 -0500

    Tarea * 5 test realizados

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+path = input("Path = ")
+        contenido = input("Contenino =")
+        arbol.setData(path,contenido)
--- zookeeper_test.py
+++ zookeeper_test.py
@@ -13,6 +13,35 @@ class TestZookeeper(unittest.TestCase):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
 
+    """
+        Tarea Hernandez Martinez Miguel Angel
+    """     
+    def test_borrar_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.delete('/node1',0)
+    
+    def test_error_al_mostrar_hijo(self):
+        with self.assertRaises(Exception):
+            tree = Ztree()
+            tree.create('/node1/node2/node3', 'algo', True, True, 10, '/')  
+            tree.getChildren()  
+    
+    def test_buscar_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.exist('/node2')
+    
+    def test_cambiar_data(self):
+        tree = Ztree()
+        tree.create('/node1/node2/node3', 'algo', True, True, 10, '/')
+        tree.setData('/node3','otra cosa')
+    
+    def test_ver_data(self):
+        tree = Ztree()
+        tree.create('/node2', 'info', True, True, 5, '/')
+        tree.getData('/node2')
+    
 
 if __name__ == '__main__':
     unittest.main()
